d853592662786938f347f0fd40cde3d8ecfa043e
dariaradko5-dev <dariaradko5@gmail.com>
2026-02-10 11:42:29 +0200

    feat(task3): Default method conflict

---
+++ src/Task3/MultiNotifier.java
@@ -0,0 +1,9 @@
+package Task3;
+
+class MultiNotifier implements SmsNotifier, EmailNotifier {
+    @Override
+    public void send(String message) {
+        SmsNotifier.super.send(message);
+        EmailNotifier.super.send(message);
+    }
+}
